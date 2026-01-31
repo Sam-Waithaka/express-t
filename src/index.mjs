9ec5f034da1bf455d7bc83e1ff8ab759dad1254d
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-01-31 16:24:17 +0300

    route params

--- src/index.mjs
+++ src/index.mjs
@@ -1,19 +1,37 @@
-import express from "express"
+import express, { response } from "express"
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
+
+const mockUsers = [
+        {id: 1, username: 'anson', displayName: 'Anson'},
+        {id: 2, username: 'anson', displayName: 'Anson'},
+        {id: 2, username: 'anson', displayName: 'Anson'},
+    ]
 
 app.get('/', (req, res)=>{
     res.status(201).send({msg: 'Hello world'})
 })
 
 app.get('/api/users', (req, res)=>{
-    res.send([
-        {id: 1, username: 'anson', displayName: 'Anson'},
-        {id: 2, username: 'anson', displayName: 'Anson'},
-        {id: 2, username: 'anson', displayName: 'Anson'},
-    ])
+    res.send(mockUsers)
+})
+
+app.get('/api/users/:id', (req, res)=>{
+    console.log(req.params);
+    const parsedId = parseInt(req.params.id)
+    console.log(parsedId);
+    
+    if (isNaN(parsedId)) return res.status(400).send({message: 'Bad request. Invalid Id'})
+    
+    const findUser = mockUsers.find(user => user.id === parsedId)
+
+    if (!findUser) return res.status(404).send({message: 'User not found'})
+    
+    return res.send(findUser)
+    
+    
 })
 
 app.get('/api/products', (req, res)=>{
