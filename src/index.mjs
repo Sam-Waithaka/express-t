040394840c5a9b5d2f2587d2841fc602e3edc66f
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-02-03 06:45:00 +0300

    Delete Request

--- src/index.mjs
+++ src/index.mjs
@@ -89,6 +89,22 @@ app.patch('/api/users/:id', (req, res)=>{
     return res.status(204).send(mockUsers)
 })
 
+app.delete('/api/users/:id', (req, res)=>{
+    const {params : {id}} = req
+    const parsedId = parseInt(id)
+
+    if (isNaN(parsedId)) return res.sendStatus(400)
+
+    const findUserIndex = mockUsers.findIndex((user)=>user.id === parsedId)
+    
+    if (findUserIndex === -1) return res.sendStatus(404)
+    
+    mockUsers.splice(findUserIndex, 1)
+
+    console.log(mockUsers);
+    return res.status(200).send(mockUsers)
+})
+
 
 app.get('/api/products', (req, res)=>{
     res.send([
