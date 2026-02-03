6d38740fae226656fd71f27ae3e638f2f50fa547
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-02-03 04:39:36 +0300

    Query params

--- src/index.mjs
+++ src/index.mjs
@@ -1,22 +1,36 @@
-import express, { response } from "express"
+import express, { request, response } from "express"
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
 const mockUsers = [
-        {id: 1, username: 'anson', displayName: 'Anson'},
-        {id: 2, username: 'anson', displayName: 'Anson'},
-        {id: 2, username: 'anson', displayName: 'Anson'},
-    ]
+    { id: 1, username: 'anson', displayName: 'Anson' },
+    { id: 2, username: 'bella', displayName: 'Bella' },
+    { id: 3, username: 'charles', displayName: 'Charles' },
+    { id: 4, username: 'diana', displayName: 'Diana' },
+    { id: 5, username: 'edwin', displayName: 'Edwin' },
+    { id: 6, username: 'faith', displayName: 'Faith' },
+    { id: 7, username: 'george', displayName: 'George' },
+    { id: 8, username: 'hannah', displayName: 'Hannah' },
+    { id: 9, username: 'ian', displayName: 'Ian' },
+    { id: 10, username: 'jane', displayName: 'Jane' }
+]
 
 app.get('/', (req, res)=>{
     res.status(201).send({msg: 'Hello world'})
 })
 
-app.get('/api/users', (req, res)=>{
-    res.send(mockUsers)
+app.get('/api/users', (req, res) => {
+    const { filter, value } = req.query
+
+    if (filter && value) {
+        return res.send(mockUsers.filter(user => user[filter].includes(value)))
+    }
+
+    return res.send(filteredUsers)
 })
+
 
 app.get('/api/users/:id', (req, res)=>{
     console.log(req.params);
